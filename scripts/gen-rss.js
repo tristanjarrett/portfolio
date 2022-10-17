2064e01bdde78b5647959e4f6f3e8fb29d1a2d3c
Tristan Jarrett <tristan.jarrett@outlook.com>
2022-10-17 09:26:49 +0100

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Tristan Jarrett',
+    site_url: 'https://tristanjarrett.com',
+    feed_url: 'https://tristanjarrett.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
